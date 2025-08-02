ee82ce7171ba9ce6723f4da83a457c7f4b5c8ba4
Kunal628-hue <creationofkunal@gmail.com>
2025-08-02 15:14:05 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,12 @@
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((l) => {
+  l.innerHTML = l.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</Span>`
+    )
+    .join("");
+});
+
